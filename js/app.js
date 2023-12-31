eceefb84eccb51bc597d3b6d3fbcbb1e313eff61
Diego Herrera Gre <diego.herrera.94@outlook.com>
2023-12-31 11:15:07 -0300

    Ajustes

--- js/app.js
+++ js/app.js
@@ -25,3 +25,21 @@ app.post("/addUser", function(req, res){
         res.end(JSON.stringify(data))
     })
 })
+
+app.get("/:id", function(req, res){
+    fs.readFile(__dirname + "/" + "users.json", 'utf8', function(err, data){
+        const users = JSON.parse(data)
+        const user = users["user" + req.params.id]
+        console.log(user)
+        res.end(JSON.stringify(user))
+    })
+})
+
+app.delete("/deleteUser", function(req, res){
+    fs.readFile(__dirname + "/" + "users.json", 'utf8', function(err, data){
+        data = JSON.parse(data)
+        delete data["user" + 2]
+        console.log(data)
+        res.end(JSON.stringify(data))
+    })
+})
